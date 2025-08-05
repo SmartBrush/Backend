1ee2d8ce712edb0393c08ab545020c6f87c53d04
heeone1 <rinakim1019@gmail.com>
2025-08-05 16:33:12 +0900

    ✨ [FEAT]
    S3 Upload

---
+++ src/main/java/com/smartbrush/smartbrush_backend/entity/DiagnosisImageEntity.java
@@ -0,0 +1,25 @@
+package com.smartbrush.smartbrush_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class DiagnosisImageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private LocalDateTime capturedAt;
+
+    private String imageUrl;  // ✅ S3에 저장된 이미지 URL
+}
